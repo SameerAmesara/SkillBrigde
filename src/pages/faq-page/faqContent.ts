901c60e185edfcd967d242becce47b529edea0ac
Dheeraj Bhat <bhat.dheeraj@gmail.com>
2024-02-27 17:45:16 -0400

    Created faq page

---
+++ src/pages/faq-page/faqContent.ts
@@ -0,0 +1,69 @@
+export const faqContentContact: { question: string; answer: string }[] = [
+  {
+    question:
+      "How do I contact customer support if I have a question or issue?",
+    answer:
+      "You can reach our customer support team by emailing support@email.com or calling our toll-free number. We're here to assist you promptly.",
+  },
+  {
+    question: "What is Lorem Ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  },
+  {
+    question: "Why do we use it?",
+    answer:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
+  },
+  {
+    question: "Where does it come from?",
+    answer:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+  },
+];
+
+export const faqContentAbout: { question: string; answer: string }[] = [
+  {
+    question: "The standard Lorem Ipsum passage, used since the 1500s",
+    answer:
+      "You can reach our customer support team by emailing support@email.com or calling our toll-free number. We're here to assist you promptly.",
+  },
+  {
+    question: "What is Lorem Ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  },
+  {
+    question: "Why do we use it?",
+    answer:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
+  },
+  {
+    question: "Where does it come from?",
+    answer:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+  },
+];
+
+export const faqContentGetStarted: { question: string; answer: string }[] = [
+  {
+    question: "The standard Lorem Ipsum passage, used since the 1500s",
+    answer:
+      "You can reach our customer support team by emailing support@email.com or calling our toll-free number. We're here to assist you promptly.",
+  },
+  {
+    question: "What is Lorem Ipsum?",
+    answer:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
+  },
+  {
+    question: "Why do we use it?",
+    answer:
+      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. ",
+  },
+  {
+    question: "Where does it come from?",
+    answer:
+      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
+  },
+];
