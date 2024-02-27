66447912fdc3a39534c8f4b05cdc34fdd137a03d
Suyash <suyashjhawer@gmail.com>
2024-02-27 19:10:19 -0400

    changes

--- src/pages/contact-us/ContactUsPage.tsx
+++ src/pages/contact-us/ContactUsPage.tsx
@@ -2,16 +2,15 @@ import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
 import Link from '@mui/material/Link';
 import Paper from '@mui/material/Paper';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+import PhoneIcon from '@mui/icons-material/Phone';
+import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-
 
 function ContactUsPage() {
   const handleSubmit = (event: { preventDefault: () => void; currentTarget: HTMLFormElement | undefined; }) => {
@@ -19,15 +18,16 @@ function ContactUsPage() {
     const data = new FormData(event.currentTarget);
     console.log({
       email: data.get('email'),
-      password: data.get('password'),
+      message: data.get('message'),
     });
+    // You can perform further actions here, such as sending the form data to a server.
   };
 
   const defaultTheme = createTheme();
 
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Grid container component="main" sx={{ height: '100vh' }}>
+      <Grid container component="main" sx={{ height: '33vh' , marginTop:'5%' }}>
         <CssBaseline />
         <Grid
           item
@@ -35,14 +35,40 @@ function ContactUsPage() {
           sm={4}
           md={7}
           sx={{
-            backgroundImage: 'url(https://source.unsplash.com/random?wallpapers)',
-            backgroundRepeat: 'no-repeat',
             backgroundColor: (t) =>
               t.palette.mode === 'light' ? t.palette.grey[50] : t.palette.grey[900],
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
+            py: 8,
+            px: 6,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
           }}
-        />
+        >
+          <Typography variant="h4" component="h2" gutterBottom>
+          Welcome to SkillBridge
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+          We're here to help you enhance your skills and connect with professionals.
+          </Typography>
+          <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+            <LocationOnIcon sx={{ mr: 1 }} />
+            <Typography variant="body1" gutterBottom>
+              SkillRidge HQ - 123 SkillRidge Ave.
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center', mb: 2 }}>
+            <PhoneIcon sx={{ mr: 1 }} />
+            <Typography variant="body1" gutterBottom>
+              555-123-456
+            </Typography>
+          </Box>
+          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+              <MailOutlineIcon sx={{ mr: 1 }}  />
+            <Typography variant="body1" gutterBottom>
+              support@skillBridge.com
+            </Typography>
+          </Box>
+        </Grid>
         <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
           <Box
             sx={{
@@ -54,10 +80,10 @@ function ContactUsPage() {
             }}
           >
             <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-              <LockOutlinedIcon />
+              <MailOutlineIcon />
             </Avatar>
             <Typography component="h1" variant="h5">
-              Sign in
+              Contact Us
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 1 }}>
               <TextField
@@ -74,15 +100,11 @@ function ContactUsPage() {
                 margin="normal"
                 required
                 fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-              <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
+                multiline
+                rows={4}
+                id="message"
+                label="Message"
+                name="message"
               />
               <Button
                 type="submit"
@@ -90,17 +112,12 @@ function ContactUsPage() {
                 variant="contained"
                 sx={{ mt: 3, mb: 2 }}
               >
-                Sign In
+                Submit
               </Button>
-              <Grid container>
-                <Grid item xs>
-                  <Link href="#" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
+              <Grid container justifyContent="flex-end">
                 <Grid item>
                   <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
+                    Back to Home
                   </Link>
                 </Grid>
               </Grid>
