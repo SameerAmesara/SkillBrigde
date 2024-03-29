b3bd5015470e2f364ce00840391b9b74ded8c3b3
Dheeraj Bhat <bhat.dheeraj@gmail.com>
2024-03-29 02:50:33 -0300

    mobx setup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,14 +6,18 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "mobx-react";
 import { router } from "./utils/routerConfig.tsx";
 import { ThemeProvider } from "@mui/material";
 import { theme } from "./utils/theme.ts";
+import { appStore } from "./mobx/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <RouterProvider router={router} />
+      <Provider store={appStore}>
+        <RouterProvider router={router} />
+      </Provider>
     </ThemeProvider>
   </React.StrictMode>
 );
