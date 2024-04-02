034353f85a36610edb996fa54972d008e05618a4
Om Anand <om.anand@dal.ca>
2024-04-02 00:05:17 -0300

    Comment out unused parser

--- backend/src/utils/parser.ts
+++ backend/src/utils/parser.ts
@@ -1,25 +1,25 @@
-import { NewUser } from '../types';
+// import { NewUser } from '../types';
 
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 
-const isString = (text: any): text is string => {
-    return typeof text === 'string' || text instanceof String;
-};
+// const isString = (text: any): text is string => {
+//     return typeof text === 'string' || text instanceof String;
+// };
 
-const parseName = (name: any): string => {
-    if (!name || !isString(name)) {
-        throw new Error(`Incorrect or missing name ${name}`);
-    }
+// const parseName = (name: any): string => {
+//     if (!name || !isString(name)) {
+//         throw new Error(`Incorrect or missing name ${name}`);
+//     }
 
-    return name;
-};
+//     return name;
+// };
 
 
-const toNewUserEntry = (object: any): NewUser => {
-    return {
-        name: parseName(object.name),
-    };
-};
+// const toNewUserEntry = (object: any): NewUser => {
+//     return {
+//         name: parseName(object.name),
+//     };
+// };
 
-export default toNewUserEntry;
+// export default toNewUserEntry;
