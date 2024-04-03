3f6c5a5e9af99f39bc97646e7bbe39f169786c13
Dheeraj Bhat <bhat.dheeraj@gmail.com>
2024-04-03 20:21:29 -0300

    User signup fix

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
-import { connectToDatabase } from './utils/config'
-import contentFeedRouter from './routes/contentFeed'
+import { connectToDatabase } from "./utils/config";
+import contentFeedRouter from "./routes/contentFeed";
 import express from "express";
 import cors from "cors";
 import middleware from "./utils/middleware";
@@ -7,24 +7,26 @@ import mentorRouter from "./routes/mentor";
 import userRouter from "./routes/userDetails";
 import discussionRouter from "./routes/discussion";
 import paymentsRouter from "./routes/payments";
-import jobRouter from './routes/job';
+import jobRouter from "./routes/job";
+import bodyParser from "body-parser";
 
 const app = express();
 
 connectToDatabase();
 
 app.use(cors());
+app.use(bodyParser.json({ limit: "10mb" }));
 app.use(express.json());
 app.use(middleware.morganMiddleWare);
 
-app.use("/contentfeed" , contentFeedRouter)
+app.use("/contentfeed", contentFeedRouter);
 app.use("/mentor", mentorRouter);
 app.use("/userDetails/", userRouter);
 app.use("/discussions", discussionRouter);
 app.use("/payments", paymentsRouter);
-app.use('/job', jobRouter)
+app.use("/job", jobRouter);
 
 app.use(middleware.errorHandler);
 app.use(middleware.unknownEndpoint);
 
-export default app
+export default app;
