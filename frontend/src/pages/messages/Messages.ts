1297ae98daeca0c7d2723e9d4e30f6a8f1506a71
Dheeraj Bhat <bhat.dheeraj@gmail.com>
2024-04-09 20:28:36 -0300

    Messaging fix

--- frontend/src/pages/messages/Messages.ts
+++ frontend/src/pages/messages/Messages.ts
@@ -1,33 +1,48 @@
-import axios from "axios"
-import { ConversationModel } from "../../models/message.model"
-import { UserDetails } from "../../models/UserDetatils.model"
+import axios from "axios";
+import { ConversationModel } from "../../models/message.model";
+import { UserDetails } from "../../models/UserDetatils.model";
 
-
-const BASE_URL = import.meta.env.VITE_BASE_URL
-const MESSAGE_URL = `${BASE_URL}/message`
-const userId = sessionStorage.getItem("userId") ?? ""
+const BASE_URL = import.meta.env.VITE_BASE_URL;
+const MESSAGE_URL = `${BASE_URL}/message`;
+const userId = sessionStorage.getItem("userId") ?? "";
 
 export const sendMessage = async (receiverId: string, message: string) => {
-    const url = `${MESSAGE_URL}/${receiverId}`
-    return await axios.post(url, { "message": message }, { headers: { "userId": userId } })
-}
+  const url = `${MESSAGE_URL}/${receiverId}`;
+  return await axios.post(
+    url,
+    { message: message },
+    { headers: { userId: userId } }
+  );
+};
 
 export const getMessages = async (receiverId: string) => {
-    const url = `${MESSAGE_URL}/${receiverId}`
-    return await axios.get(url, { headers: { "userId": userId } })
-}
+  const url = `${MESSAGE_URL}/${receiverId}`;
+  return await axios.get(url, { headers: { userId: userId } });
+};
 
 export const getConversations = async () => {
-    const url = `${BASE_URL}/networking/userconnections`
-    const userConnections = (await axios.get(url, { params: { "uid": userId } })).data.myConnections as string[]
-    
-    let conversations: ConversationModel[] = []
-    userConnections.forEach(async connectionId => {
-        const userDetails = (await axios.get<UserDetails>(`${BASE_URL}/userDetails/${connectionId}`)).data
-        conversations.push({
-            receiverId: connectionId,
-            recieverName: userDetails.firstName + " " + userDetails.lastName
-        })
-    })
-    return conversations
-}
+  const url = `${BASE_URL}/networking/userconnections`;
+  try {
+    const response = await axios.get(url, { params: { uid: userId } });
+    const userConnections = response.data.myConnections as string[];
+
+    const conversationsPromises = userConnections.map(
+      async (connectionId): Promise<ConversationModel> => {
+        const userDetailsResponse = await axios.get<UserDetails>(
+          `${BASE_URL}/userDetails/${connectionId}`
+        );
+        const userDetails = userDetailsResponse.data;
+        return {
+          receiverId: connectionId,
+          recieverName: userDetails.firstName + " " + userDetails.lastName,
+        };
+      }
+    );
+
+    const conversations = await Promise.all(conversationsPromises);
+    return conversations;
+  } catch (error) {
+    console.error("Failed to get conversations:", error);
+    return [];
+  }
+};
