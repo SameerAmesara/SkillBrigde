be2404c914393c8521bcd03872093af192745d52
Dheeraj Bhat <bhat.dheeraj@gmail.com>
2024-04-10 03:19:34 -0300

    Transaction fixes

--- frontend/src/pages/transactions-page/TransactionsPage.tsx
+++ frontend/src/pages/transactions-page/TransactionsPage.tsx
@@ -57,16 +57,17 @@ const TransactionsPage = observer(() => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {isTransactionsLoading ? (
-              [1, 2].map((value, index) => (
+            {!isTransactionsLoading ? (
+              [1, 2, 3, 4, 5, 6, 7].map((value, index) => (
                 <TableRow key={value + index}>
                   <TableCell
                     colSpan={4}
                     align="center"
                     component="th"
                     padding="none"
+                    sx={{ padding: "5px" }}
                   >
-                    <Skeleton height={60}></Skeleton>
+                    <Skeleton variant="rectangular" height={45}></Skeleton>
                   </TableCell>
                 </TableRow>
               ))
